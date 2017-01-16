d7d593d608cf67d5fed656c3bcde02be3d1cc02b
veinhorn <b.korogvich@gmail.com>
2017-01-16 21:47:47 +0300

    updated ver, little fix

--- src/main/java/com/veinhorn/tikiticket/core/api/ICredentials.java
+++ src/main/java/com/veinhorn/tikiticket/core/api/ICredentials.java
@@ -2,6 +2,7 @@ package com.veinhorn.tikiticket.core.api;
 
 /**
  * Created by veinhorn on 18.12.16.
+ * Provides credentials for authentication on web portal
  */
 public interface ICredentials {
     String getLogin();
